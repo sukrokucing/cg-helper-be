26ff34c2b399b2b07394dd88c3fa2bd28b8ff816
jimmy <jimmy@dwp.co.id>
2025-06-01 12:40:46 +0700

    add vercel

---
+++ api/index.js
@@ -0,0 +1,12 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+// Required for Vercel
+module.exports = (req, res) => {
+  server(req, res);
+};
